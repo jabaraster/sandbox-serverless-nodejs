5047bec393c568080c93fc234eba3291fe5ab155
jabaraster <ah@jabara.info>
2016-10-19 23:20:34 +0900

    mainte.

---
+++ README.md
@@ -0,0 +1,4 @@
+## デプロイコマンド
+```sh
+sls deploy && osascript -e 'display notification "deploy finished." with title "Serverless"'
+```
--- dynamo.js
+++ dynamo.js
@@ -5,13 +5,25 @@ const dynamodb = new AWS.DynamoDB({region: 'ap-northeast-1'});
 
 module.exports = {
     insert: (event, context, callback) => {
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify({
-                func: 'insert',
-            }),
+        const body = JSON.parse(event.body);
+        const param = {
+            TableName: `${event.requestContext.stage}-comments`,
+            Item: {
+                id: { "S": body.id },
+                user_id: { "S": body.user_id },
+            },
         };
-        callback(null, response);
+        dynamodb.putItem(param, (err, data) => {
+            const response = {
+                statusCode: 201,
+                body: JSON.stringify({
+                    func: 'insert',
+                    error: err ? err : null,
+                    data: data ? data : null,
+                }),
+            };
+            callback(null, response);
+        });
     },
 
     select: (event, context, callback) => {
